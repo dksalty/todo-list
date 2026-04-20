24c4b39403677747081ae1fbcade71bbeafeca21
dksalty <rollinmelson@gmail.com>
2026-04-19 21:59:24 -0400

    Lined up .days and Main with grid and adjusted colors

--- src/index3.js
+++ src/index3.js
@@ -6,6 +6,18 @@ const anytimeDiv = document.getElementById('anytime')
 
 const appendArray = getTodos();
 
+const daySelect = {
+today: todayDiv,
+tomorrow: tomorrowDiv,
+comingUp: comingUpDiv,
+anytime: anytimeDiv 
+}
+
+todayDiv.textContent = '';
+tomorrowDiv.textContent = '';
+comingUpDiv.textContent = '';
+anytimeDiv.textContent = '';
+
 appendArray.forEach((todo) => {
     const newTodo = document.createElement('div');
     newTodo.classList.add('addedTodo');
@@ -19,7 +31,10 @@ appendArray.forEach((todo) => {
     todoInfo.textContent = todo.description;
     timeInfo.textContent = todo.time;
     
+
     newTodo.append(projectTitle, priorityLevel, todoInfo, timeInfo);
+    
+   daySelect[todo.day].appendChild(newTodo);
 
 
 });
